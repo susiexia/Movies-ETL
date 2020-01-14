8d475f421b6f56cf6ba9d2f659ef8d89823e14c3
Jiqian Xia <susiexia@Jiqians-MBP.lan>
2020-01-15 00:14:00 +0800

    uodate data-cleaning code

--- wiki_json.py
+++ wiki_json.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np 
 
 # %%
-# EXTRACT process
+# ----------EXTRACT process-------------------------
 # load Wiki source (json file) into python, use f_string
 file_dir = '/Users/susiexia/desktop/module_8/Movies-ETL/raw_data'
 # use WITH statement to open and read file
@@ -22,5 +22,22 @@ ratings_df = pd.read_csv(f'{file_dir}/ratings.csv')
 kaggle_metadata_df.sample(n=5)
 
 kaggle_metadata_df.tail()
+
+# %%
+# -----------TRANSFORM process-------------------------
+# wiki source tranform into DataFrame to inspect
+wiki_movies_df = pd.DataFrame(wiki_movies_raw)
+wiki_movies_df.head()
+wiki_movies_df.columns.tolist()
+#wiki_movies_df.index
+
+# %%
+# filter wiki datasets by only contains director and Imdb information
+# in original list of dictionary use list comprehension
+wiki_movies = [movie for movie in wiki_movies_raw 
+                if ('Directed by' in movie or 'Director' in movie) 
+                and ('imdb_link' in movie)
+                and ('No. of episodes' not in movie) ]
+len(wiki_movies)
 
 # %%
